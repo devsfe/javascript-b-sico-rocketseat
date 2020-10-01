1aa8572cacb3d9d3db3eb77c558bd8c17ba7faec
Fernando Chaves <fernando.chaves16@hotmail.com>
2020-09-30 21:45:13 -0300

    Conteúdo aula variáveis

---
+++ 1.variáveis/script.js
@@ -0,0 +1,15 @@
+var nome = "Fernando"; //string
+var idade = 18; //inteiro
+var peso = 60.4; //float
+var humano = true; //boolean
+var alunos = ['Fernando', 'Isaque', 'Lucas']; //vetores
+
+var aluno = { //objetos
+    nome: alunos[2],
+    idade: 18,
+    peso: 60.4,
+    humano: true
+};
+
+console.log(aluno.nome);
+
