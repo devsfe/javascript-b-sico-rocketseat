bbff9b46fbfbe4a5dca444a8d279406cb8e97cae
Fernando Chaves <fernando.chaves16@hotmail.com>
2020-10-05 23:02:49 -0300

    Exercicios 1 e 2 completos

--- MÓDULO 1/EXERCICIOS/script.js
+++ MÓDULO 1/EXERCICIOS/script.js
@@ -1,16 +1,35 @@
 // 1º exercício
 
+function exibeMensagem() {
+    return (
+        "O usuário mora em " +
+        endereco.cidade +
+        " / " +
+        endereco.uf +
+        ", no bairro " +
+        endereco.bairro +
+        ', na rua "' +
+        endereco.rua +
+        '" com nº ' +
+        endereco.numero +
+        "."
+    );
+}
+
 var endereco = {
     rua: "Rua dos pinheiros",
     numero: 1293,
     bairro: "Centro",
-    cidade: "São Paulo",
+        cidade: "São Paulo",
     uf: "SP"
-};
+};  
 
+console.log(exibeMensagem());
 
 // 2º exercício
 // Crie uma função que dado um intervalo (entre x e y) exiba todos número pares
+
+console.log ('Exercicio 2')
 
 function pares (x, y) {
     for (var i = x; i <= y; i++) {
@@ -22,6 +41,6 @@ function pares (x, y) {
 
 pares(20, 39);
 
-// 2º exercício
+// 3º exercício
 // Crie uma função que dado um intervalo (entre x e y) exiba todos número pares
 
